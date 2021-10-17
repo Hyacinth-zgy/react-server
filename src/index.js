3d98af5e34393e3e6f487f0c272315a35f339c4d
Hyacinth-zgy <838673371@qq.com>
2021-10-17 15:10:01 +0800

    配置组件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import React from 'react';
 import Home from './containers/Home';
+// 这里引入react提供用于服务端渲染的方法renderToString，将react组件转换为字符串
 import {renderToString} from 'react-dom/server'
 const app = express();
+// 传入react组件
 const content= renderToString(<Home />);
 console.log(content)
 app.get('/',function(req,res) {
